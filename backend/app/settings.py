71e7cefb9b635f8522e22e9a25d9acc36b940f83
VyacheslavKuchumov <Kuchumov76@mail.ru>
2025-07-01 17:05:50 +0500

    Update settings.py

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -33,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['https://django-test.vyachik-dev.ru', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://django-test.vyachik-dev.ru', 'localhost', '127.0.0.1', '0.0.0.0', '*']
 
 INTERNAL_IPS = [
     # ...
